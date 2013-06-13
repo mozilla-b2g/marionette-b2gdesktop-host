96a3338839e62d72aeb33409957daa624185b58d
James Lal <james@lightsofapollo.com>
2013-06-12 17:20:11 -0700

    Add a better example cleanup code

--- examples/index.js
+++ examples/index.js
@@ -1,16 +1,63 @@
 var Marionette = require('marionette-client'),
-    host = require('../index');
+    host = require('../index'),
+    static = require('node-static'),
+    http = require('http');
 
 var B2G_ROOT = __dirname + '/b2g';
+var HTTP_PORT = 60034;
 
-host.spawn('b2g', B2G_ROOT, function(err, port, child) {
-  setTimeout(function() {
-    var driver = new Marionette.Drivers.Tcp({ port: port });
-    driver.connect(function() {
-      var client = new Marionette.Client(driver);
-      client.startSession(function() {
-        console.log('WOOT SHIT WORKS!!');
+// create server
+var file = new(static.Server)(__dirname + '/public');
+var server = require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    //
+    // Serve files!
+    //
+    file.serve(request, response);
+  }).resume();
+}).listen(HTTP_PORT);
+
+var driver;
+var client;
+var b2gProcess;
+
+// creates connection to marionette
+function connect(port) {
+  driver = new Marionette.Drivers.Tcp({ port: port });
+  driver.connect(function() {
+    createSession();
+  });
+}
+
+// initiates session for tests to use
+function createSession() {
+  client = new Marionette.Client(driver, {
+    defaultCallback: function() {}
+  });
+
+  client.startSession(function() {
+    invokeTests();
+  });
+}
+
+// do somethings with marionette
+function invokeTests() {
+  client.goUrl('http://localhost:' + HTTP_PORT);
+  client.findElement('#show-secret', function(err, el) {
+    el.click();
+    client.findElement('#secret', function(err, el) {
+      el.text(function(err, text) {
+        console.log('[marionette] secret: "%s"', text);
+        client.deleteSession(function() {
+          b2gProcess.kill();
+          server.close();
+        });
       });
     });
-  }, 5000);
+  });
+}
+
+host.spawn(B2G_ROOT, { product: 'b2g' }, function(err, port, child) {
+  b2gProcess = child;
+  connect(port);
 });
